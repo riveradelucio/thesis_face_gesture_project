cc7f0b899a6c2cee1484f04e25a7b7557592b822
Rodrigo Rivera <rodrigo.rivera@urbvan.com>
2025-05-12 02:06:59 -0600

    gesture | cover eyes with animation

--- app/gesture_recognition.py
+++ app/gesture_recognition.py
@@ -2,12 +2,17 @@ import cv2
 import mediapipe as mp
 import math
 
+# Initialize MediaPipe
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=2)
 mp_face = mp.solutions.face_mesh
 face = mp_face.FaceMesh()
 finger_tips = [4, 8, 12, 16, 20]
+
+# Face landmark IDs
 MOUTH_TOP = 13
+LEFT_EYE_POINTS = [33, 145, 159]
+RIGHT_EYE_POINTS = [263, 374, 386]
 LEFT_EAR_POINTS = [234, 93]
 RIGHT_EAR_POINTS = [454, 323]
 
@@ -19,6 +24,8 @@ def detect_custom_gesture(frame):
 
     all_hands = []
     mouth_pos = None
+    left_eye_positions = []
+    right_eye_positions = []
     left_ear_positions = []
     right_ear_positions = []
 
@@ -28,8 +35,13 @@ def detect_custom_gesture(frame):
             h, w, _ = frame.shape
             for i, lm in enumerate(faceLms.landmark):
                 cx, cy = int(lm.x * w), int(lm.y * h)
+
                 if i == MOUTH_TOP:
                     mouth_pos = (cx, cy)
+                if i in LEFT_EYE_POINTS:
+                    left_eye_positions.append((cx, cy))
+                if i in RIGHT_EYE_POINTS:
+                    right_eye_positions.append((cx, cy))
                 if i in LEFT_EAR_POINTS:
                     left_ear_positions.append((cx, cy))
                 if i in RIGHT_EAR_POINTS:
@@ -45,22 +57,19 @@ def detect_custom_gesture(frame):
                 lm_list.append((id, cx, cy))
             all_hands.append(lm_list)
 
-    # Gesture detection
     if all_hands:
         if len(all_hands) == 1:
             return _detect_one_hand(all_hands[0], mouth_pos, left_ear_positions, right_ear_positions)
         elif len(all_hands) == 2:
-            return _detect_two_hands(all_hands[0], all_hands[1])
+            return _detect_two_hands(all_hands[0], all_hands[1], left_eye_positions, right_eye_positions)
     return None
 
 def _detect_one_hand(lm_list, mouth_pos, left_ear, right_ear):
-    # Check finger states
-    fingers = [1 if lm_list[4][2] < lm_list[3][2] else 0]  # Thumb up
+    fingers = [1 if lm_list[4][2] < lm_list[3][2] else 0]  # Thumb
     for tip_id in finger_tips[1:]:
         fingers.append(1 if lm_list[tip_id][2] < lm_list[tip_id - 2][2] else 0)
     total_fingers = sum(fingers)
 
-    # Thumbs up: thumb up, all other fingers down
     if fingers[0] == 1 and all(f == 0 for f in fingers[1:]):
         return "Thumbs_Up"
 
@@ -80,7 +89,6 @@ def _detect_one_hand(lm_list, mouth_pos, left_ear, right_ear):
         avg_x = (index_finger[1] + middle_tip[1]) / 2
         avg_y = (index_finger[2] + middle_tip[2]) / 2
         dist_to_mouth = math.hypot(avg_x - mouth_pos[0], avg_y - mouth_pos[1])
-
         if fingers_distance < 40 and avg_y > mouth_pos[1] and dist_to_mouth < 100:
             return "Eat"
 
@@ -99,7 +107,24 @@ def _detect_one_hand(lm_list, mouth_pos, left_ear, right_ear):
 
     return None
 
-def _detect_two_hands(hand1, hand2):
+def _detect_two_hands(hand1, hand2, left_eye_pos, right_eye_pos):
+    # Get relevant finger tips
+    tips1 = [x for x in hand1 if x[0] in [8, 12, 16]]  # index, middle, ring
+    tips2 = [x for x in hand2 if x[0] in [8, 12, 16]]
+
+    # Cover Eyes gesture: check if any finger from each hand is close to left/right eyes
+    if left_eye_pos and right_eye_pos:
+        for tip1 in tips1:
+            for (ex, ey) in left_eye_pos:
+                dist1 = math.hypot(tip1[1] - ex, tip1[2] - ey)
+                if dist1 < 60:
+                    for tip2 in tips2:
+                        for (ex2, ey2) in right_eye_pos:
+                            dist2 = math.hypot(tip2[1] - ex2, tip2[2] - ey2)
+                            if dist2 < 60:
+                                return "Cover_eyes"
+
+    # Heart gesture
     thumb1 = next((x for x in hand1 if x[0] == 4), None)
     thumb2 = next((x for x in hand2 if x[0] == 4), None)
     pinky1 = next((x for x in hand1 if x[0] == 20), None)
@@ -111,4 +136,4 @@ def _detect_two_hands(hand1, hand2):
         if thumb_dist < 60 and pinky_dist < 60:
             return "Heart"
 
-    return "Medicine"
+    return None
