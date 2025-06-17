a45aa50f5c3766760786becb31f9484f29e79e1d
Rodrigo Rivera <rodrigo.rivera@urbvan.com>
2025-06-17 09:45:35 -0600

    Cover Eyes gesture | The sign for cover eyes is now working better. Now, the thumbs up and down are not working as well.

--- app/gesture_recognition.py
+++ app/gesture_recognition.py
@@ -115,8 +115,6 @@ def _detect_one_hand(lm_list, mouth_pos, left_ear, right_ear):
     if lm_list[4][2] > lm_list[3][2] and all(lm_list[tip_id][2] > lm_list[tip_id - 2][2] for tip_id in finger_tips[1:]):
         return "Thumbs_Down"
 
-    # "Fist" and "Peace" gestures removed
-
     index_finger = next((x for x in lm_list if x[0] == 8), None)
     middle_tip = next((x for x in lm_list if x[0] == 12), None)
 
@@ -144,14 +142,18 @@ def _detect_one_hand(lm_list, mouth_pos, left_ear, right_ear):
 def _detect_two_hands(hand1, hand2, left_eye_pos, right_eye_pos):
     tips1 = [x for x in hand1 if x[0] in [8, 12, 16]]
     tips2 = [x for x in hand2 if x[0] in [8, 12, 16]]
+
     if left_eye_pos and right_eye_pos:
-        for tip1 in tips1:
-            for (ex, ey) in left_eye_pos:
-                if math.hypot(tip1[1] - ex, tip1[2] - ey) < 60:
-                    for tip2 in tips2:
-                        for (ex2, ey2) in right_eye_pos:
-                            if math.hypot(tip2[1] - ex2, tip2[2] - ey2) < 60:
-                                return "Cover_eyes"
+        left_detected = any(
+            math.hypot(t1[1] - ex, t1[2] - ey) < 80
+            for t1 in tips1 for (ex, ey) in left_eye_pos
+        )
+        right_detected = any(
+            math.hypot(t2[1] - ex, t2[2] - ey) < 80
+            for t2 in tips2 for (ex, ey) in right_eye_pos
+        )
+        if left_detected and right_detected:
+            return "Cover_eyes"
 
     thumb1 = next((x for x in hand1 if x[0] == 4), None)
     thumb2 = next((x for x in hand2 if x[0] == 4), None)
